a9acc7673356f761e3572c1231e0bfc05c4f8ba3
Caterina-Perrucci <caterina.perrucci@aulab.it>
2024-07-04 10:33:09 +0200

    Scaffolding completato

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-12">
+                <h1>Libreria</h1>
+            </div>
+        </div>
+    </div>
+</x-layout>
